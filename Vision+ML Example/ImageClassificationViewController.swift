543a3566aea9c10238a3ccab69bd07a0c6473769
ArushiA <arushi.sparkle@gmail.com>
2020-03-01 11:50:28 -0500

    Image Classifier

--- Vision+ML Example/ImageClassificationViewController.swift
+++ Vision+ML Example/ImageClassificationViewController.swift
@@ -23,11 +23,11 @@ class ImageClassificationViewController: UIViewController {
     lazy var classificationRequest: VNCoreMLRequest = {
         do {
             /*
-             Use the Swift class `MobileNet` Core ML generates from the model.
+             Use the Swift class `Model' Core ML generates from the model.
              To use a different Core ML classifier model, add it to the project
              and replace `MobileNet` with that model's generated Swift class.
              */
-            let model = try VNCoreMLModel(for: MobileNet().model)
+            let model = try VNCoreMLModel(for: Recycling().model)
             
             let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
                 self?.processClassifications(for: request, error: error)
